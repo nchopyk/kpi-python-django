66782ba1a7f81b493fe3d346dadc942360a439c2
Nazar Chopyk <61637619+naz-olegovich@users.noreply.github.com>
2021-10-02 15:56:06 +0300

    Added export to databases (#7)
    * Added MySQL container
    
    * Rewriting to 3 db tables
    
    * Code refactoring
    
    * Changed postgres user
    
    * Rewriting to 3 db tables
    
    * Functions for generating sql
    
    * Api for mysql
    
    * Changed logs text
    
    * Added export to db 2 and db 3
    
    * Move to another folder
    
    * Bug fixed
    
    Co-authored-by: Nazar Chopyk <>

---
+++ Lab1/Server/db_tools.py
@@ -0,0 +1,78 @@
+def remove_unnecessary_columns(columns_to_keep, row, skip_id=False):
+    all_columns = ("id", 'name', 'brand', 'size', 'energy_efficiency_class', 'electricity_costs_per_year', 'price')
+
+    columns_to_keep = list(columns_to_keep)
+    if "id" not in columns_to_keep and not skip_id: columns_to_keep.insert(0, "id")
+
+    row_to_insert = []
+    for column in columns_to_keep:
+        row_to_insert.append(row[all_columns.index(column)])
+
+    return row_to_insert
+
+
+def on_conflict_do_update(columns, syntax='postgres'):
+    fields = []
+    if syntax == 'postgres':
+        for column in columns:
+            fields.append(f"{column}=EXCLUDED.{column}")
+        return f"ON CONFLICT (id) DO UPDATE SET {', '.join(fields)}"
+    elif syntax == 'mysql':
+        for column in columns:
+            fields.append(f"{column}={column}")
+        return f"ON DUPLICATE KEY UPDATE {', '.join(fields)}"
+    else:
+        return ""
+
+
+def generate_technique_insert_row_sql(columns_to_keep, row, syntax="postgres"):
+    all_technique_columns = ['id', 'name', 'brand']
+    technique_columns_to_keep = [column for column in all_technique_columns if column in columns_to_keep or column == "id"]
+    technique_row_to_insert = remove_unnecessary_columns(technique_columns_to_keep, row)
+
+    if syntax == 'postgres':
+        sql = f"INSERT INTO technique ({', '.join(technique_columns_to_keep)}) VALUES {tuple(technique_row_to_insert)} " \
+              f"{on_conflict_do_update(technique_columns_to_keep, syntax='postgres')};"
+    elif syntax == "mysql":
+        sql = f"INSERT INTO technique ({', '.join(technique_columns_to_keep)}) VALUES {tuple(technique_row_to_insert)} " \
+              f"{on_conflict_do_update(technique_columns_to_keep, syntax='mysql')};"
+    else:
+        sql = f"INSERT INTO technique ({', '.join(technique_columns_to_keep)}) VALUES {tuple(technique_row_to_insert)};"
+
+    return sql
+
+
+def generate_specifications_insert_row_sql(columns_to_keep, row, syntax='postgres'):
+    all_specifications_columns = ['id', 'size', 'energy_efficiency_class', 'technique_id']
+    specifications_columns_to_keep = [column for column in all_specifications_columns if column in columns_to_keep or column == "id"]
+    specifications_row_to_insert = remove_unnecessary_columns(specifications_columns_to_keep, row)
+    specifications_row_to_insert.append(row[0])
+
+    if syntax == 'postgres':
+        sql = f"INSERT INTO specifications ({', '.join(specifications_columns_to_keep)}, technique_id) VALUES {tuple(specifications_row_to_insert)} " \
+              f"{on_conflict_do_update(specifications_columns_to_keep, syntax='postgres')};"
+    elif syntax == "mysql":
+        sql = f"INSERT INTO specifications ({', '.join(specifications_columns_to_keep)}, technique_id) VALUES {tuple(specifications_row_to_insert)} " \
+              f"{on_conflict_do_update(specifications_columns_to_keep, syntax='mysql')};"
+    else:
+        sql = f"INSERT INTO specifications ({', '.join(specifications_columns_to_keep)}, technique_id) VALUES {tuple(specifications_row_to_insert)};"
+
+    return sql
+
+
+def generate_prices_insert_row_sql(columns_to_keep, row, syntax='postgres'):
+    all_prices_columns = ['id', 'electricity_costs_per_year', 'price']
+    prices_columns_to_keep = [column for column in all_prices_columns if column in columns_to_keep or column == "id"]
+    prices_row_to_insert = remove_unnecessary_columns(prices_columns_to_keep, row)
+    prices_row_to_insert.append(row[0])
+
+    if syntax == 'postgres':
+        sql = f"INSERT INTO prices ({', '.join(prices_columns_to_keep)}, technique_id) VALUES {tuple(prices_row_to_insert)} " \
+              f"{on_conflict_do_update(prices_columns_to_keep, syntax='postgres')};"
+    elif syntax == "mysql":
+        sql = f"INSERT INTO prices ({', '.join(prices_columns_to_keep)}, technique_id) VALUES {tuple(prices_row_to_insert)} " \
+              f"{on_conflict_do_update(prices_columns_to_keep, syntax='mysql')};"
+    else:
+        sql = f"INSERT INTO prices ({', '.join(prices_columns_to_keep)}, technique_id) VALUES {tuple(prices_row_to_insert)};"
+
+    return sql
--- Lab1/Server/mysql_db.py
+++ Lab1/Server/mysql_db.py
@@ -1,42 +1,40 @@
-import psycopg2
+import mysql.connector
 import db_tools
 
 
-class PostgresDb:
+class MysqlDb:
     def __init__(self):
-        self.connection = psycopg2.connect(
-            database="postgres",
-            user="admin",
+        self.connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
             password="admin",
-            host="127.0.0.1",
-            port="5432",
+            database="mysql"
         )
         self.cursor = self.connection.cursor()
-        print("PostgreSQL: connection successful")
+        print("MySQL: connection successful")
 
         self._create_technique_table()
         self._create_specifications_table()
         self._create_prices_table()
 
-    def import_records(self, columns, records):
-        for row in records:
-            print("PostgreSQL: beginning transaction")
-            try:
-                sql_add_technique = db_tools.generate_technique_insert_row_sql(columns, row)
-                sql_add_specifications_to_technique = db_tools.generate_specifications_insert_row_sql(columns, row)
-                sql_add_prices_to_technique = db_tools.generate_prices_insert_row_sql(columns, row)
+    def import_record(self, columns, records):
+        print("MySQL: beginning transaction")
+        last_row = records[-1]
 
-                self.cursor.execute(sql_add_technique)
-                self.cursor.execute(sql_add_specifications_to_technique)
-                self.cursor.execute(sql_add_prices_to_technique)
+        try:
+            sql_add_technique = db_tools.generate_technique_insert_row_sql(columns, last_row, syntax='mysql')
+            sql_add_specifications_to_technique = db_tools.generate_specifications_insert_row_sql(columns, last_row, syntax='mysql')
+            sql_add_prices_to_technique = db_tools.generate_prices_insert_row_sql(columns, last_row, syntax='mysql')
 
-                self.connection.commit()
+            self.cursor.execute(sql_add_technique)
+            self.cursor.execute(sql_add_specifications_to_technique)
+            self.cursor.execute(sql_add_prices_to_technique)
 
-                print('PostgreSQL: row was successfully exported to Postgres')
-            except psycopg2.DatabaseError:
-                print("PostgreSQL: transaction failed, rollback")
+            self.connection.commit()
 
-        print('PostgreSQL: records were successfully exported to Postgres')
+            print('MySQL: row was successfully exported to MySQL')
+        except mysql.connector.Error:
+            print("Mysql: transaction failed, rollback")
 
     def get_all_records(self):
         sql_get_all_records = """   SELECT technique.id, technique.name, technique.brand, specifications.size,
@@ -56,7 +54,7 @@ class PostgresDb:
         sql_create_technique_table = """CREATE TABLE IF NOT EXISTS technique (id INTEGER PRIMARY KEY, name TEXT, brand TEXT);"""
         self.cursor.execute(sql_create_technique_table)
         self.connection.commit()
-        print("PostgreSQL: technique table created (or already exists)")
+        print("MySQL: technique table created (or already exists)")
 
     def _create_specifications_table(self):
         sql_create_specifications_table = """CREATE TABLE IF NOT EXISTS specifications (
@@ -67,7 +65,7 @@ class PostgresDb:
                                              FOREIGN KEY(technique_id) REFERENCES technique(id) ON DELETE CASCADE);"""
         self.cursor.execute(sql_create_specifications_table)
         self.connection.commit()
-        print("PostgreSQL: specifications table created (or already exists)")
+        print("MySQL: specifications table created (or already exists)")
 
     def _create_prices_table(self):
         sql_create_prices_table = """CREATE TABLE IF NOT EXISTS prices (
@@ -78,7 +76,7 @@ class PostgresDb:
                                      FOREIGN KEY(technique_id) REFERENCES technique(id) ON DELETE CASCADE); """
         self.cursor.execute(sql_create_prices_table)
         self.connection.commit()
-        print("PostgreSQL: prices table created (or already exists)")
+        print("MySQL: prices table created (or already exists)")
 
     def _remove_none_from_records(self, records):
         records_to_return = []
